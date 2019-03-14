1bfd8445e60df5c6704ce46ce6ddb8488a494ff7
jbribiesca <bribiesca@gmail.com>
2019-03-14 12:46:36 -0500

    initial commit

---
+++ assets/javascript/code.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyCPQDVSecaDgGUmNrQrr85VoxOp7xuaoag",
+    authDomain: "jbribiescadb.firebaseapp.com",
+    databaseURL: "https://jbribiescadb.firebaseio.com",
+    projectId: "jbribiescadb",
+    storageBucket: "jbribiescadb.appspot.com",
+    messagingSenderId: "970609866074"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
